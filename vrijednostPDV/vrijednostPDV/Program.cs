c1d88f431840052a87f9703f6b7b0f5a99b528f8
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-10-13 13:46:03 +0200

    Add files via upload

---
+++ vrijednostPDV/vrijednostPDV/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite cijenu bez PDV-a: ");
+        decimal cijenaBezPDV = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Unesite cijenu s PDV-om: ");
+        decimal cijenaSPDV = Convert.ToDecimal(Console.ReadLine());
+
+        decimal pdv = cijenaSPDV - cijenaBezPDV;
+        decimal pdvPostotak = (pdv / cijenaBezPDV) * 100;
+
+        Console.WriteLine($"Izračunati PDV: {pdv} kn");
+        Console.WriteLine($"Postotak PDV-a: {pdvPostotak}%");
+    }
+}
